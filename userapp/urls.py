8416c3665a4312a64de98466473df5ca8abc7125
Rithin Ashikh <rithinashikhev@gmail.com>
2023-09-09 12:46:57 +0530

    commented unwanted

--- userapp/urls.py
+++ userapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView,UserCreateAPIView,UserOnlyCreateAPIView, CustomTokenObtainPairSerializer
+from .views import CustomTokenObtainPairSerializer
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -8,8 +8,8 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('api/login/', TokenObtainPairView.as_view(serializer_class=CustomTokenObtainPairSerializer), name='token_obtain_pair'),
     path('api/login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/register/', RegisterView.as_view(), name="sign_up"),
-    path('api/user/create/', UserCreateAPIView.as_view(), name='user-create'),
-    path('api/user-only/create/', UserOnlyCreateAPIView.as_view(), name='user-create'),
+    # path('api/register/', RegisterView.as_view(), name="sign_up"),
+    # path('api/user/create/', UserCreateAPIView.as_view(), name='user-create'),
+    # path('api/user-only/create/', UserOnlyCreateAPIView.as_view(), name='user-create'),
 ]
 
