413f2aa125ccc9904e70e29f07a0bbba8a6d225c
Rithin Ashikh <rithinashikhev@gmail.com>
2023-09-12 15:08:59 +0530

    latest3 changed

--- netmagics/urls.py
+++ netmagics/urls.py
@@ -3,6 +3,6 @@ from .views import AcivityTrackerView
 
 
 urlpatterns = [
-    path('activity_tracker/list', AcivityTrackerView.as_view(), name='ActivityTrackerSerializer'),
+    path('activity-tracker/list', AcivityTrackerView.as_view(), name='ActivityTrackerSerializer'),
 ]
 
